0bf65fedcec75b5952cd151ec41b8710921ef706
ErosTenshi <christianangel0711@gmail.com>
2025-03-31 21:47:06 -0400

    Added third person perspective, Main Menu screen and implemented Project7 Material.

--- SpaceJam.py
+++ SpaceJam.py
@@ -13,10 +13,15 @@ from direct.gui.DirectGui import DirectFrame, DirectButton
 class MyGame(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        # Sets up Universe model and texture
+        
+        self.main_menu = DirectFrame(frameColor=(0, 0, 0, 1), frameSize=(-1, 1, -1, 1))
+        self.start_button = DirectButton(text="Start Game", scale=0.1, pos=(0, 0, 0.2), command=self.start_game, parent=self.main_menu)
+        self.quit_button = DirectButton(text="Quit Game", scale=0.1, pos=(0, 0, -0.2), command=sys.exit, parent=self.main_menu)
+        
+    def start_game(self):
+        self.main_menu.hide()
         self.Universe = spaceJamClasses.Universe(self.loader, "Assets/Universe/Universe.x", self.render, 'Universe', "Assets/Universe/Universe.jpg", (0, 0, 0), 15000)
         
-        # Sets up Planet 1 model, texture, and location/position
         self.Planet1 = spaceJamClasses.Planet(self.loader, "Assets/Planets/protoPlanet.x", self.render, 'Planet1', "Assets/Planets/PlanetPsy.png", (150, 5000, 67), 350)
         
         self.Planet2 = spaceJamClasses.Planet(self.loader, "Assets/Planets/protoPlanet.x", self.render, 'Planet2', "Assets/Planets/PlanetYinYang.png", (1150, 5000, 1067), 350)
@@ -29,15 +34,29 @@ class MyGame(ShowBase):
         
         self.Planet6 = spaceJamClasses.Planet(self.loader, "Assets/Planets/protoPlanet.x", self.render, 'Planet6', "Assets/Planets/PlanetSangre.png", (5150, 5000, 5067), 350)
         
-        # Sets up Space Station model, texture, and location/position
+        
         self.SpaceStation = spaceJamClasses.SpaceStation(self.loader, "Assets/SpaceStation/spaceStation.x", self.render, 'Space Station', "Assets/SpaceStation/SpaceStation1_Dif2.png", (1500, 1000, -100), 40)
 
+    
+        
         self.cTrav = CollisionTraverser()
         self.cTrav.traverse(self.render)
-        # Sets up Spaceship model, texture, and location/position.
+        
         self.SpaceShip = SpaceShip.Player(self.cTrav, self.loader, self.taskMgr, self.accept, "Assets/Spaceships/spacejet.3ds", self.render, 'Space Ship', "Assets/Spaceships/spacejet_C.png", Vec3(1000, 1200, -550), 50, self.render)
         self.SpaceShip.SetKeyBindings()
         
+        self.is_first_person = True
+        self.accept('p', self.ThirdPerson) #Keybinds for First and Third Person Perspectives
+        self.accept('o', self.FirstPerson)
+        self.setup_game_elements()
+        self.FirstPerson()
+        
+        self.Sentinal1 = spaceJamClasses.Orbiter(self.loader, self.taskMgr, "Assets/DroneDefender/DroneDefender.obj", self.render, "Drone", 6.0, "Assets/DroneDefender/octotoad1_auv.png", self.Planet5, 900, "MLB", self.SpaceShip)
+        self.Sentinal2 = spaceJamClasses.Orbiter(self.loader, self.taskMgr, "Assets/DroneDefender/DroneDefender.obj", self.render, "Drone", 6.0, "Assets/DroneDefender/octotoad1_auv.png", self.Planet2, 500, "Cloud", self.SpaceShip)
+        self.Sentinal3 = spaceJamClasses.Orbiter(self.loader, self.taskMgr, "Assets/DroneDefender/DroneDefender.obj", self.render, "Drone", 6.0, "Assets/DroneDefender/octotoad1_auv.png", self.Planet3, 900, "MLB", self.SpaceShip)
+        self.Sentinal4 = spaceJamClasses.Orbiter(self.loader, self.taskMgr, "Assets/DroneDefender/DroneDefender.obj", self.render, "Drone", 6.0, "Assets/DroneDefender/octotoad1_auv.png", self.Planet4, 500, "Cloud", self.SpaceShip)
+        
+    def setup_game_elements(self):
         self.Drone = spaceJamClasses.Drone(self.loader, "Assets/DroneDefender/DroneDefender.x", self.render, 'Drone', "Assets/DroneDefender/Drones.jpg", (1000, 1200, 0), 50)
         
         self.CircleDrone = self.loader.loadModel("Assets/DroneDefender/DroneDefender.x", 1)
@@ -58,6 +77,7 @@ class MyGame(ShowBase):
         self.cancel_button = DirectButton(text="Cancel", scale=0.07, pos=(0, 0, -0.1), command=self.hide_exit_menu, parent=self.exit_menu)
         self.exit_menu.hide()
         self.accept("escape", self.show_exit_menu)
+        
           
         x = 0
         for i in range(105):
@@ -98,7 +118,7 @@ class MyGame(ShowBase):
 
             self.DrawCloudDefense(self.Planet1, nickName)
             nickName = "Drone" + str(spaceJamClasses.Drone.droneCount + 1)
-            self.DrawBaseballSeams(self.SpaceStation, nickName, j, fullCycle, 2)
+            self.DrawBaseballSeams(self.SpaceStation, nickName, j, fullCycle, 2)    
         
     def show_exit_menu(self):
         self.exit_menu.show()
@@ -116,9 +136,7 @@ class MyGame(ShowBase):
         
     def update_droneExplodeCount(self):
         SpaceShip.Player.droneExplodeCount += 1
-        self.droneExplodeCount_text.setText(f"Drones Destroyed: {SpaceShip.Player.droneExplodeCount}")
-        
-    
+        self.droneExplodeCount_text.setText(f"Drones Destroyed: {SpaceShip.Player.droneExplodeCount}")    
         
     def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, radius = 1):
         unitVec = defensePaths.BaseballSeams(step, numSeams, B = 0.4)
@@ -132,17 +150,22 @@ class MyGame(ShowBase):
         position = unitVec * 500 + centralObject.modelNode.getPos()
         spaceJamClasses.Drone(self.loader, "Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "Assets/DroneDefender/octotoad1_auv.png", position, 10)
     
-    def SetCamera(self):
+    def FirstPerson(self):
         self.disableMouse()
+        self.is_first_person
+            # First-person mod
         self.camera.reparentTo(self.SpaceShip.modelNode)
-        self.camera.setFluidPos(0, 1, 0)
+        self.camera.setFluidPos(0, 1, 0)  # Adjust to a first-person view
+        
+    def ThirdPerson(self):
+        self.camera.reparentTo(self.SpaceShip.modelNode)
+        self.camera.setPos(self.SpaceShip.modelNode.getPos() + Vec3(-10, -10, 0))  # Set camera position behind the ship
+        self.camera.setFluidPos(0, -20, 5)
+
     
-        
-        
         
     def quit(self):
         sys.exit()  
          
 app = MyGame()
-app.SetCamera()
 app.run()
